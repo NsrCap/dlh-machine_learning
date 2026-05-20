8b6e7163e9b4d02d79acc37a60810cb1ebb9f6f6
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 19:13:48 +0000

    matrix multiplication using importing numpy module

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this task is about matrix multiplication using import module and numpy."""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """this is documentation for matrix multiplication."""
+    new_matrix = np.matmul(mat1, mat2)
+    return new_matrix
