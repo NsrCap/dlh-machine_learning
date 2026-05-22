0a45b6129852baad61be1c7a5102867eed5b7dc9
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-22 23:38:59 +0000

    determinant of matrix, require minor matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,12 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 def determinant(matrix):
+    # check matrix type including rows
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a list of lists")
-    if matrix[[]]:
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a list of lists")
+
+    # special case
+    if matrix == [[]]:
         return 1
+
+    # check square
     for row in matrix:
         if len(row) != len(matrix):
-            raise TypeError("matrix must be a square matrix")
+            raise ValueError("matrix must be a square matrix")
+
+    # base case
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
@@ -15,6 +26,8 @@ def determinant(matrix):
         c = matrix[1][0]
         d = matrix[1][1]
         return a*d - b*c
+
+    # recursive case
     det = 0
 
     for j in range(len(matrix)):
@@ -25,5 +38,9 @@ def determinant(matrix):
                 if c != j:
                     row.append(matrix[r][c])
             minor.append(row)
-        det += ((-1) ** j) * matrix[0][j] * determinant(minor)
-    return det
+        sign = (-1) ** j
+        value = matrix[0][j]
+        sub_det = determinant(minor)
+        det += sign * value * sub_det
+
+return det
