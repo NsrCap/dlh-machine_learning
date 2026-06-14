d02b7448cff353611d150f91c364c335fd3f905a
nsrcap <nsr.capital.007@gmail.com>
2026-06-14 02:00:27 +0200

    finding possion distribution for discrete random variable, default lambdha is 5, which is mean or average rate of occurence of event/outcome for fixed internal of time/space. if no observation data point provided then lambdha will be 5

---
+++ math/probability/poisson.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This module creates a class Poisson"""
+
+
+class Poisson:
+    """This class represents a poisson distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """constructor method."""
+        self.data = data
+        self.lambtha = float(lambtha)
+        if not lambtha >= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                Sum = 0
+                for i in range(len(data)):
+                    Sum += data[i]
+            self.lambtha = Sum / len(data)
